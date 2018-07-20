556f6c099f003dd843e0e3dff528c760001a0207
YogrtMan <yifan.kou@hotmail.com>
2018-07-19 22:13:06 -0400

    implemented date passing functionality

--- app/src/main/java/notificationorganizer/yogrtman/com/notificationorganizer/TaskList/NewTaskActivity.kt
+++ app/src/main/java/notificationorganizer/yogrtman/com/notificationorganizer/TaskList/NewTaskActivity.kt
@@ -2,29 +2,49 @@ package notificationorganizer.yogrtman.com.notificationorganizer.TaskList
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.DatePicker
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_new_task.*
 import kotlinx.android.synthetic.main.toolbar_app_action_bar.*
 import notificationorganizer.yogrtman.com.notificationorganizer.R
 import notificationorganizer.yogrtman.com.notificationorganizer.Utils.AppBarManager
 import notificationorganizer.yogrtman.com.notificationorganizer.Utils.TimePickerFragment
+import java.text.DateFormat
+import java.util.*
 
 class NewTaskActivity : AppCompatActivity() {
+    val TAG: String = "NewTasKActivity";
+
+    companion object {
+        val LABEL_SELECTED_DATE: String = "DATE";
+    }
+
+    lateinit var mDate: Date;
+
+    lateinit var btnPickTime: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_task);
         setSupportActionBar(appBar);
 
+        mDate = Date(intent.getLongExtra(LABEL_SELECTED_DATE, 0));
+        Log.d(TAG, "Date: " + mDate.toString());
+
+        btnPickTime = findViewById<Button>(R.id.btnPickTime);
         btnPickTime.setOnClickListener(View.OnClickListener {
             var fragment: DialogFragment = TimePickerFragment();
             fragment.show(supportFragmentManager, "timePicker");
         })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/notificationorganizer/yogrtman/com/notificationorganizer/TaskList/TaskCalendarPage/CalendarFragment.kt
+++ app/src/main/java/notificationorganizer/yogrtman/com/notificationorganizer/TaskList/TaskCalendarPage/CalendarFragment.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import notificationorganizer.yogrtman.com.notificationorganizer.R
 import notificationorganizer.yogrtman.com.notificationorganizer.TaskList.NewTaskActivity
+import java.util.*
 
 class CalendarFragment : Fragment() {
+    val TAG: String = "CalendarFragment";
+
+    lateinit var calendarView: CalendarView;
+    lateinit var fabNewTask: FloatingActionButton;
+
+    var mHighlightedDate: Calendar = Calendar.getInstance();
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -20,11 +29,21 @@ class CalendarFragment : Fragment() {
         val rootView: View = inflater.inflate(
                 R.layout.fragment_task_calendar, container, false);
 
-        rootView.findViewById<FloatingActionButton>(R.id.fabNewTask).setOnClickListener { view ->
+        fabNewTask = rootView.findViewById<FloatingActionButton>(R.id.fabNewTask);
+        fabNewTask.setOnClickListener { view ->
             var intent = Intent(context, NewTaskActivity::class.java)
+
+            intent.putExtra(NewTaskActivity.LABEL_SELECTED_DATE, mHighlightedDate.time.time);
             startActivity(intent);
         }
 
+        calendarView = rootView.findViewById<CalendarView>(R.id.calendar);
+        calendarView.setOnDateChangeListener {view, year, month, dayOfMonth ->
+            Log.d(TAG, "Calendar highlighted " + year + "-" + month + "-" + dayOfMonth);
+            mHighlightedDate.set(Calendar.YEAR, year);
+            mHighlightedDate.set(Calendar.MONTH, month);
+            mHighlightedDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        }
         return rootView
     }
 
